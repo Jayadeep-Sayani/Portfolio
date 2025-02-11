019436d9ca01e5093c0bed4543b447194d858227
Jayadeep_Sayani <jayadeeps1101@gmail.com>
2025-02-11 15:50:03 -0800

    updated the skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,10 +3,124 @@ import React from 'react'
 export const Skills = () => {
   return (
     <div className='skills' id="skills">
-        <h1>Skills</h1>
-        <div className="line-with-diamond">
-            <div className="diamond"></div>
-        </div>
+      <h1>Skills</h1>
+      <div className="line-with-diamond">
+        <div className="diamond"></div>
+      </div>
+
+      <div className="frontend">
+        <h2>Frontend</h2>
+        <ul>
+          <li>
+            <img src="https://cdn.worldvectorlogo.com/logos/html-1.svg" alt="HTML" />
+            <h5>HTML</h5>
+          </li>
+          <li>
+            <img src="https://img.icons8.com/?size=512&id=21278&format=png" alt="CSS" />
+            <h5>CSS</h5>
+          </li>
+          <li>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/768px-JavaScript-logo.png" alt="JavaScript" />
+            <h5>JavaScript</h5>
+          </li>
+          <li>
+            <img src="https://www.svgrepo.com/show/452092/react.svg" alt="React" />
+            <h5>React</h5>
+          </li>
+          <li>
+            <img src="https://www.svgrepo.com/show/374118/tailwind.svg" alt="Tailwind" />
+            <h5>Tailwind</h5>
+          </li>
+          <li>
+            <img src="https://www.svgrepo.com/show/353751/flutter.svg" alt="Flutter" />
+            <h5>Flutter</h5>
+          </li>
+        </ul>
+      </div>
+
+
+      <div className="backend">
+        <h2>Backend</h2>
+        <ul>
+          <li>
+            <img src="https://www.svgrepo.com/show/184143/java.svg" alt="Java" />
+            <h5>Java</h5>
+          </li>
+          <li>
+            <img src="https://cdn.iconscout.com/icon/free/png-256/free-python-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-vol-5-pack-logos-icons-3030224.png?f=webp&w=256" alt="Python" />
+            <h5>Python</h5>
+          </li>
+          <li>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png" alt="PHP" />
+            <h5>PHP</h5>
+          </li>
+          <li>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1853px-C_Programming_Language.svg.png" alt="C" />
+            <h5>C</h5>
+          </li>
+          <li>
+            <img src="https://www.svgrepo.com/show/353735/firebase.svg" alt="Firebase DB" />
+            <h5>Firebase</h5>
+          </li>
+          <li>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Bash_Logo_Colored.svg/2048px-Bash_Logo_Colored.svg.png" alt="Bash" />
+            <h5>Bash</h5>
+          </li>
+          <li>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Godot_icon.svg/2048px-Godot_icon.svg.png" alt="Godot Game Engine" />
+            <h5>Godot</h5>
+          </li>
+        </ul>
+      </div>
+
+      <div className="tools">
+        <h2>Tools</h2>
+        <ul>
+          <li>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png" alt="VS Code" />
+            <h5>VS Code</h5>
+          </li>
+          <li>
+            <img src="https://www.svgrepo.com/show/373623/git.svg" alt="Git" />
+            <h5>Git</h5>
+          </li>
+          <li>
+            <img src="https://developer.fedoraproject.org/static/logo/cli-app.png" alt="CLI" />
+            <h5>CLI</h5>
+          </li>
+          <li>
+            <img src="https://cdn.sanity.io/images/599r6htc/regionalized/46a76c802176eb17b04e12108de7e7e0f3736dc6-1024x1024.png" alt="Figma" />
+            <h5>Figma</h5>
+          </li>
+        </ul>
+      </div>
+
+      <div className="soft-skills">
+        <h2>Soft Skills</h2>
+        <ul>
+          <li>
+            <img src="https://www.svgrepo.com/show/452228/problem-solving.svg" alt="Problem-solving" />
+            <h5>Problem-solving</h5>
+          </li>
+          <li>
+            <img src="https://www.svgrepo.com/show/452204/communication.svg" alt="Communication" />
+            <h5>Communication</h5>
+          </li>
+          <li>
+            <img src="https://www.svgrepo.com/show/452209/teamwork.svg" alt="Teamwork" />
+            <h5>Teamwork</h5>
+          </li>
+          <li>
+            <img src="https://www.svgrepo.com/show/452205/adaptability.svg" alt="Adaptability" />
+            <h5>Adaptability</h5>
+          </li>
+          <li>
+            <img src="https://www.svgrepo.com/show/452206/time-management.svg" alt="Time Management" />
+            <h5>Time Management</h5>
+          </li>
+        </ul>
+      </div>
+
     </div>
   )
 }
