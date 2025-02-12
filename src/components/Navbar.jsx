15c34603741e5d46fde9684ffe7450367c8d02b2
Jayadeep_Sayani <jayadeeps1101@gmail.com>
2025-02-11 22:20:08 -0800

    made nav bar responsive - 99% done protfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useMediaQuery } from '@mui/material';
+import { Menu, X } from 'lucide-react';
 
 export const Navbar = () => {
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <div className='navbar'>
-
-        <ul>
-            <li><a href='#home'>Home</a></li>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#experience'>Experience</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#skills'>Skills</a></li>
-            <li><a href='#contact'>Contact</a></li>
+      {isMobile ? (
+        <div className='mobile-nav'>
+          <button className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? <X size={44} /> : <Menu size={44} />}
+          </button>
+          {menuOpen && (
+            <ul className='nav-links mobile-menu'>
+              <li><a href='#home' onClick={handleLinkClick}>Home</a></li>
+              <li><a href='#about' onClick={handleLinkClick}>About</a></li>
+              <li><a href='#experience' onClick={handleLinkClick}>Experience</a></li>
+              <li><a href='#projects' onClick={handleLinkClick}>Projects</a></li>
+              <li><a href='#skills' onClick={handleLinkClick}>Skills</a></li>
+              <li><a href='#contact' onClick={handleLinkClick}>Contact</a></li>
+            </ul>
+          )}
+        </div>
+      ) : (
+        <ul className='nav-links'>
+          <li><a href='#home'>Home</a></li>
+          <li><a href='#about'>About</a></li>
+          <li><a href='#experience'>Experience</a></li>
+          <li><a href='#projects'>Projects</a></li>
+          <li><a href='#skills'>Skills</a></li>
+          <li><a href='#contact'>Contact</a></li>
         </ul>
+      )}
     </div>
-  )
-}
+  );
+};
